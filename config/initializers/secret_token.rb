2d4fc956e05de16aff05a242dcd675cc3b1f62e0
Serena Wales <serenawr@gmail.com>
2012-03-26 16:31:40 -0700

    Initial Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openblight::Application.config.secret_token = 'a5fa1d8c8a29b255a30702cc36d410ceda7902be8655cc0e4b762a0e0b153ae369e17c6408af2d5e92005959544982b0c5e46b58fd854fcdaf1730600a5253bd'
