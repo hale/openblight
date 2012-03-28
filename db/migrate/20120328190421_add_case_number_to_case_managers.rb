8170a579eadfa1fe3ac668224372e910a999156a
Eddie A Tejeda <eddie@codeforamerica.org>
2012-03-28 16:46:28 -0700

    fix migrate error which prevented postgres from loading data properly

--- db/migrate/20120328190421_add_case_number_to_case_managers.rb
+++ db/migrate/20120328190421_add_case_number_to_case_managers.rb
@@ -2,6 +2,6 @@ class AddCaseNumberToCaseManagers < ActiveRecord::Migration
   def change
     add_column :case_managers, :case_number, :string
     add_column :case_managers, :name, :string
-    remove_column :case_managers, :case, :string
+    remove_column :case_managers, :case
   end
 end
