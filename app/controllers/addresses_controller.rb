6a62e869e2fbfa0057fee7249e686e85b67b8ec4
Serena Wales <serenawr@gmail.com>
2012-04-09 16:22:10 -0700

    Basic search

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,9 +1,36 @@
 class AddressesController < ApplicationController
+  respond_to :html, :xml, :json
+
   def index
-    @address = Address.all
+    @address = Address.all.order(:address_long)
+
+    respond_with(@address)
   end
 
   def show
     @address = Address.find(params[:id])
+
+    respond_with(@address)
+  end
+  
+  def search
+    search = params[:address]
+    long_addr_regex = /[1-9]+\s?([a-zA-Z ]+)(st|ave|dr|ct|rd|ln|pl|park|blvd|aly)/i
+    if long_addr_regex =~ search
+      search = search.upcase
+      @addresses = Address.where("address_long = ?", search).order(:house_num)
+    else
+      no_num_regex = /([a-zA-Z ]+)(st|ave|dr|ct|rd|ln|pl|park|blvd|aly)/i
+      match = no_num_regex.match(search)
+      if match
+        search = match[1].strip.upcase
+      end
+      @addresses = Address.where("street_name = ?", search).order(:house_num)
+      if @addresses.empty?
+        @addresses = Address.where("address_long LIKE ?", "%#{search}%").order(:house_num)
+      end
+    end
+
+    respond_with(@addresses)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Openblight::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  match "addresses/search" => "addresses#search"
   resources :addresses, :except => [:destroy, :create, :edit]
 
   # Sample resource route with options:
