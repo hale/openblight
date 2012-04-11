cae8e0d9be777f4d265b917d219ecd6a3619a1e7
Serena Wales <serenawr@gmail.com>
2012-04-11 16:38:07 -0700

    Upload multiple resets pdf

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120410232248) do
+ActiveRecord::Schema.define(:version => 20120411185913) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "geopin"
@@ -149,6 +149,7 @@ ActiveRecord::Schema.define(:version => 20120410232248) do
     t.string   "case_number"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.datetime "reset_date"
   end
 
 end
