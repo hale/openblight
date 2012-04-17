308c41d5bad18fb6d60958f4f36d5cda40d6a372
Serena Wales <serenawr@gmail.com>
2012-04-17 12:29:50 -0700

    Address ID can be validated

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ActiveRecord::Base
 	has_many :cases
 
-  #validates_uniqueness_of :address_id
+  validates_uniqueness_of :address_id
 	#validates_uniqueness_of :parcel_id
   #validates_uniqueness_of :geopin
 
