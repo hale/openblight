e709261dc3a4b2c26dc60e846b56169eb39d6abf
Serena Wales <serenawr@gmail.com>
2012-05-09 15:39:22 -0700

    Validation on reset

--- app/models/reset.rb
+++ app/models/reset.rb
@@ -1,6 +1,8 @@
 class Reset < ActiveRecord::Base
 	belongs_to :case, :foreign_key => :case_number, :primary_key => :case_number
 
+  validates_uniqueness_of :reset_date, :scope => :case_number
+
   def date
     self.reset_date || DateTime.new(0)
   end
