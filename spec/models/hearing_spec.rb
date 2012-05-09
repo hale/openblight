0801951b5070b02c2fad65dc042b29145f999fd7
Serena Wales <serenawr@gmail.com>
2012-05-09 12:18:48 -0700

    Test hearing validation

--- spec/models/hearing_spec.rb
+++ spec/models/hearing_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe Hearing do
   it { should belong_to(:case) }
+
+  it "should not create multiple hearings for the same date with the same case" do
+    time = Time.now
+    c = FactoryGirl.create(:case)
+
+    Hearing.create(:case_number => c.case_number, :hearing_date => time)
+    Hearing.create(:case_number => c.case_number, :hearing_date => time)
+
+    Hearing.count.should eq 1
+  end
 end
