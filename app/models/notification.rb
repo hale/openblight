40a9dd721018ab66ef66d73bf20f78a7dde6c9aa
Serena Wales <serenawr@gmail.com>
2012-05-11 13:21:34 -0700

    Missing validation for notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,7 @@
 class Notification < ActiveRecord::Base
 	belongs_to :case, :foreign_key => :case_number, :primary_key => :case_number
+  
+  validates_uniqueness_of :notified, :scope => [:case_number, :notification_type]
 
   def date
     self.notified.to_datetime || DateTime.new(0)
