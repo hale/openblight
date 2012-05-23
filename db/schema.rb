908fd6ec9ffe05f8a6dc0622b284b35d23d676ee
Amir Reavis-Bey <amirbey@gmail.com>
2012-05-22 19:46:50 -0700

    update statistics to mvc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120514204730) do
+ActiveRecord::Schema.define(:version => 20120522175756) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "geopin"
@@ -185,6 +185,11 @@ ActiveRecord::Schema.define(:version => 20120514204730) do
   create_table "searches", :force => true do |t|
     t.text     "term"
     t.string   "ip"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "statistics", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
