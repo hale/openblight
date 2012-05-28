61444f78da78a3a61fb608827ef27225428a7477
Eddie A Tejeda <eddie@codeforamerica.org>
2012-05-28 01:45:19 -0500

    adjustments to styles

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -5,7 +5,7 @@ module AddressesHelper
       if a.foreclosures != [] || a.demolitions != [] || a.maintenances != []
         progress = {:bar => '100%', :arrow => '97%', :class => "full"}
       elsif a.judgements != []
-        progress = {:bar => '80%', :arrow => '78%'}
+        progress = {:bar => '79%', :arrow => '77%'}
       elsif a.hearings != []
         progress = {:bar => '60%', :arrow => '58%'}
       elsif a.notifications != []
