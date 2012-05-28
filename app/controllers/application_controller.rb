753222ae82951eb45247235ba49d2e8aecb5e6b0
Eddie A Tejeda <eddie@codeforamerica.org>
2012-05-28 01:40:30 -0500

    detect cookie for legal disclaimer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :handle_cookies
+  private
+    def handle_cookies
+      @agree_to_legal_disclaimer = !cookies[:agree_to_legal_disclaimer].nil?
+    end
+
+
 end
+
